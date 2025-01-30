ec28a55ae3e7f170251158b6f72c8bbdb7d1d805
Benson221655 <seerbenson@gmail.com>
2025-01-30 16:43:22 -0600

    size changes, alignment fixes, bug fixes

--- TestAR/ARViewContainer.swift
+++ TestAR/ARViewContainer.swift
@@ -38,17 +38,14 @@ struct ARViewContainer: UIViewRepresentable {
         @objc func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
             guard let arView = gestureRecognizer.view as? ARView else { return }
             let tapLocation = gestureRecognizer.location(in: arView)
-            let results = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+            let results = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any)
             if let firstResult = results.first {
-                var location = simd_make_float3(firstResult.worldTransform.columns.3)
+                let location = simd_make_float3(firstResult.worldTransform.columns.3)
                 if dotLocations.count % 2 == 1 {
                     let connectionAnchor = connectDots(position1: dotLocations[dotLocations.count - 1], position2: location)
                     arView.scene.addAnchor(connectionAnchor)
                 }
-                else {
-                    dotLocations.append(location)
-                }
-                location.y += 0.025
+                dotLocations.append(location)
                 let sphere = createSphere()
                 let objectAnchor = AnchorEntity(world: location)
                 objectAnchor.addChild(sphere)
@@ -57,7 +54,7 @@ struct ARViewContainer: UIViewRepresentable {
         }
         
         func createSphere() -> ModelEntity {
-            let sphere = MeshResource.generateSphere(radius: 0.05)
+            let sphere = MeshResource.generateSphere(radius: 0.0025)
             let material = SimpleMaterial(color: .red, isMetallic: false)
             let entity = ModelEntity(mesh: sphere, materials: [material])
             return entity
@@ -65,23 +62,17 @@ struct ARViewContainer: UIViewRepresentable {
         
         func connectDots(position1: simd_float3, position2: simd_float3) -> AnchorEntity {
             let midPosition = SIMD3(x:(position1.x + position2.x) / 2, y:(position1.y + position2.y) / 2, z:(position1.z + position2.z) / 2)
-                
-            let anchor = AnchorEntity()
-            anchor.position = midPosition
-            anchor.look(at: position1, from: midPosition, relativeTo: nil)
-            
+            let connectionAnchor = AnchorEntity()
+            connectionAnchor.position = midPosition
+            connectionAnchor.look(at: position1, from: midPosition, relativeTo: nil)
             let meters = simd_distance(position1, position2)
+            let connectionMaterial = SimpleMaterial.init(color: .red, roughness: 1, isMetallic: false)
+            let connectionMesh = MeshResource.generateBox(width:0.00175, height: 0.00175/4, depth: meters)
+            let connectionEntity = ModelEntity(mesh: connectionMesh, materials: [connectionMaterial])
+            connectionEntity.position = .init(0, 0.00175, 0)
+            connectionAnchor.addChild(connectionEntity)
             
-            let lineMaterial = SimpleMaterial.init(color: .red, roughness: 1, isMetallic: false)
-            
-            let bottomLineMesh = MeshResource.generateBox(width:0.025, height: 0.025/2.5, depth: meters)
-            
-            let bottomLineEntity = ModelEntity(mesh: bottomLineMesh, materials: [lineMaterial])
-            
-            bottomLineEntity.position = .init(0, 0.025, 0)
-            anchor.addChild(bottomLineEntity)
-            
-            return anchor
+            return connectionAnchor
         }
     }
 }
